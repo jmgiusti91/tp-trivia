e97e9838950cf3a9fe9ef530f3f4b97106fb69bd
jmgiusti91 <jmgiusti91@gmail.com>
2016-09-09 22:47:37 -0300

    Se finalizo estilo de primer pregunta. Se cambiaron otros estilos, se agregaron imagenes de fondo y alguna funcionalidad como deshabilitar boton una vez que se respondio, esperar 3 seg hasta cargar la otra pregunta etc.

--- tp-trivia/www/js/controllers.js
+++ tp-trivia/www/js/controllers.js
@@ -1,28 +1,69 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
-  $scope.preguntas = {};
-  $scope.preguntas.uno = " Cual fue el primer campeon invicto del futbol argentino?";
+.controller('DashCtrl', function($scope, $timeout) {
+  $scope.usuario = {};
+  $scope.preguntas = " Cual fue el primer campeon invicto del futbol argentino?";
   $scope.Ver = false;
+  $scope.respuestas = {};
+  $scope.respuestas.uno = "A - San Lorenzo";
+  $scope.respuestas.dos = "B - River";
+  $scope.respuestas.tres = "C - Boca";
+  $scope.respuestas.cuatro = "D - Racing";
+  $scope.instancia = 1;
+  $scope.eligio = false;
 
   $scope.Respuesta=function(rta){
-    console.log(rta);
-    $("#ciclon").removeClass("button-positive");
-    $("#ciclon").addClass("resp-correcta");
-    if(rta == 'b'){
-      $("#river").removeClass("button-positive");
-      $("#river").addClass("resp-incorrecta");
-    } else if(rta == 'c'){
-      $("#boca").removeClass("button-positive");
-      $("#boca").addClass("resp-incorrecta");
-    } else if(rta == 'd'){
-      $("#racing").removeClass("button-positive");
-      $("#racing").addClass("resp-incorrecta");
+      console.log(rta);
+      $scope.eligio = true;
+      if($scope.instancia == 1){
+      $("#uno").removeClass("button-positive");
+      $("#uno").addClass("resp-correcta");
+      if(rta == 'b'){
+        $("#dos").removeClass("button-positive");
+        $("#dos").addClass("resp-incorrecta");
+      } else if(rta == 'c'){
+        $("#tres").removeClass("button-positive");
+        $("#tres").addClass("resp-incorrecta");
+      } else if(rta == 'd'){
+        $("#cuatro").removeClass("button-positive");
+        $("#cuatro").addClass("resp-incorrecta");
+      }
     }
+      
+
+    $timeout(function(){
+      $scope.eligio = false;
+      if($scope.instancia == 1){
+      $("#uno").removeClass("resp-correcta");
+      $("#uno").addClass("button-positive");
+      $("#dos").removeClass("resp-incorrecta");
+      $("#dos").addClass("button-positive");
+      $("#tres").removeClass("resp-incorrecta");
+      $("#tres").addClass("button-positive");
+      $("#cuatro").removeClass("resp-incorrecta");
+      $("#cuatro").addClass("button-positive");
+
+      $scope.preguntas = " Quien descubrio America?";
+      $scope.respuestas.uno = "A - San Martin";
+      $scope.respuestas.dos = "B - Monguito";
+      $scope.respuestas.tres = "C - Colon";
+      $scope.respuestas.cuatro = "D - Susana Gimenez";
+      $scope.instancia = 2;
+      }
+    }, 3000)
+    
+    /*$scope.preguntas = " Quien descubrio America?";
+      $scope.respuestas.uno = "A - San Martin";
+      $scope.respuestas.dos = "B - Monguito";
+      $scope.respuestas.tres = "C - Colon";
+      $scope.respuestas.cuatro = "D - Susana Gimenez";*/
+    
   }
 
   $scope.Empezar=function(){
     $scope.Ver = true;
+    $("#ion-cont").removeClass("fondo-inicio");
+    $("#ion-cont").addClass("fondo-preg");
   }
 
 })
